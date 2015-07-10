9b20b5068b7552ac2e3d53af35eee3432914a89a
tcanipel789 <thomas.canipel@gmail.com>
2015-07-10 18:14:17 +0800

    v1
    v1

---
+++ online.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var fs = require ("fs");
+var app = express();
+
+
+
+
+app.use(express.static('public'));
+
+app.get("/players",function(req,res){
+	res.sendfile("./public/htm/players.html");
+});
+
+
+
+
+app.get("/search",function(req,res){
+	console.log(req.query);
+	
+	res.send(req.query);
+});
+
+app.post("/form",function(req,res){
+	console.log(req.tostring());
+	req.urlencoded("index.html");
+});
+
+app.get('/books', function (req, res) {
+  fs.readFile(process.argv[3], function (err, data) {
+    if (err) {
+      throw err;
+    }
+    object = JSON.parse(data);
+    res.send(object);
+  });
+});
+
+app.put("/:ID/:NAME",function(req,res){
+	console.log(req.params.NAME);
+	//console.log(req.params.ID);
+	
+});
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Online app listening at http://%s:%s', host, port);
+
+});;
---
+++ public/js/devicesCtrl.js
@@ -0,0 +1,58 @@
+app.controller('deviceCtrl', function($scope) {
+$scope.isVisible = false;
+$scope.status = '';
+$scope.name = '';
+$scope.tags = '';
+$scope.dynamicPopover = {
+    content: 'Hello, World!',
+    title: 'Title'
+  };
+
+
+$scope.devices = [
+{id:'1', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'2', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'3', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'4', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'5', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'6', status:'offline', name:'i5-test',  tags: 'test'},
+{id:'7', status:'offline', name:'i5-test',  tags: 'test'}
+];
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false;
+
+$scope.editDevice = function(id) {
+  $scope.isVisible = true;
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    } else {
+    $scope.edit = false;
+    $scope.status = $scope.devices[id-1].status;
+    $scope.name = $scope.devices[id-1].name;
+	$scope.tags = $scope.devices[id-1].tags;
+  }
+};
+
+$scope.openForm = function(){
+	
+};
+
+$scope.save = function() {
+ $scope.isVisible = false;
+};
+
+$scope.$watch('status',function() {$scope.test();});
+$scope.$watch('name', function() {$scope.test();});
+$scope.$watch('tags', function() {$scope.test();});
+
+$scope.test = function() {
+  $scope.incomplete = false;
+  if ($scope.edit && (!$scope.name.length ||
+  !$scope.status.length)) {
+       $scope.incomplete = true;
+  }
+};
+
+});
