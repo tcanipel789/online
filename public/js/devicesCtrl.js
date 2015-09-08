96e4afa2fc6425dbdd64f047777c1acbc776d0e6
tcanipel789 <thomas.canipel@gmail.com>
2015-09-08 10:38:07 +0800

    improve display

--- public/js/devicesCtrl.js
+++ public/js/devicesCtrl.js
@@ -11,15 +11,63 @@ $scope.error = false;
 $scope.incomplete = false;
 $scope.listVisible = true;
 
+$scope.stylereload = "glyphicon glyphicon-refresh";
+
+
+
+ // array for dropped items
+    $scope.dropped = [];
+ 
+    // array of items for dragging
+    $scope.items = [
+        {id: 1, name: "Microwave"}, 
+        {id: 2, name: "Dishwasher" },
+        {id: 3, name: "Phone" },
+        {id: 4, name: "Punching Bag" }
+    ];
+ 
+    $scope.moveToBox = function(id) {
+ 
+        for (var index = 0; index < $scope.items.length; index++) {
+ 
+            var item = $scope.items[index];
+                 
+            if (item.id == id) {
+                // add to dropped array
+                $scope.dropped.push(item);
+ 
+                // remove from items array
+                $scope.items.splice(index, 1);
+            }
+        }
+ 
+        $scope.$apply();
+    };
+ 
+    $scope.showItmesLeft = function () {
+        alert($scope.items.length + " items left.");
+    };
+     
+    $scope.showItmesDropped = function () {
+        alert($scope.dropped.length + " items in drop-box.");
+    };
+
+
+
+
 $scope.reload = function() {
+
 	if ($scope.playerVisible == true){
-		
+	$scope.stylereload = "glyphicon glyphicon-transfer";
+	
 	$http.get('/online/devices').
 	  success(function(data, status, headers, config) {
 		$scope.devices = data;
+		$scope.stylereload = "glyphicon glyphicon-refresh";
 	  }).
 	  error(function(data, status, headers, config) {
 	   console.log("error when retrieving devices");
+	   $scope.stylereload = "glyphicon glyphicon-refresh";
 	  });
   }
 }
